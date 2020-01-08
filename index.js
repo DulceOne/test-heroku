fb6323cedb6ab87e8f3287c901600c712a9e444d
DulceOne <Dulce322228@gmail.com>
2020-01-08 21:34:43 +0200

    Fix port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.get('/', (req,res) => {
     res.end('<h1>Hello world!!!</h1>')
 })
@@ -9,6 +9,6 @@ app.post('/test', (req, res) => {
     res.send({message: 'work pls'});
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("SERVER IS STARTED...")
 })
